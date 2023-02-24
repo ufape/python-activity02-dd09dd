b830c51d239b29f6e7eb361eecd5cab4efcbd87e
dd09dd <125400026+dd09dd@users.noreply.github.com>
2023-02-24 16:00:12 -0300

    Add files via upload

--- problem_1.py
+++ problem_1.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
 # YOUR FULL NAME
 # UAG00098
@@ -12,7 +12,7 @@ Input(s):
 Quatro números inteiros v1, v2, v3 e v4.
 Exemplo 1:
 Digite o valor A: 5
-Digite o valor B: 6
+Digite o valor A: 6
 Digite o valor C: 7
 Digite o valor D: 8
 Exemplo 2:
@@ -24,7 +24,7 @@ Digite o valor D: 6
 Processes:
 Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos.", senão escrever "Valores recusados.".
 
-Output(s):
+Output(s): 
 Mostre a respectiva mensagem após a validação dos valores.
 Exemplo 1:
 Valores recusados.
@@ -32,12 +32,18 @@ Exemplo 2:
 Valores aceitos.
 
 
-"""
+""" 
 
 
 def main():
-    pass  # your code here
-
-
+ A = int(input('Digite o valor A: '))
+ B = int(input('Digite o valor B: '))
+ C = int(input('Digite o valor C: '))
+ D = int(input('Digite o valor D: '))
+  
+ if B > C and D > A and (C + D) > (A + B) and C > 0 and D > 0 and A % 2 == 0:
+    print("Valores aceitos.")
+ else:
+    print("Valores recusados.")
 if __name__ == '__main__':
     main()
